4968a8974e9475e672a9b88d845f61fba1c8c584
Julimorg <kienphongtran2003@gmail.com>
2025-02-22 21:03:49 +0700

    config theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography  from '@mui/material/Typography'
 
 function App() {
   return (
     <>
     <div>TranKienPhong</div>
+    <Typography variant="body2" color = "text.secondary">Hello, world!</Typography>
     <Button variant="text">Text</Button>
     <Button variant="contained">Contained</Button>
     <Button variant="outlined">Outlined</Button>
     <AccessAlarmIcon/>
     <ThreeDRotation/>
+  
     </>
   )
 }
